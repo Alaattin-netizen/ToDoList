ce303b24e6e17fd508711113aef157ffde6a3c1f
Alaattin-netizen <alaattin.yilmaz302@gmail.com>
2026-08-19 15:46:50 +0300

    taskstore changes

--- app/stores/taskStore.ts
+++ app/stores/taskStore.ts
@@ -1,78 +1,64 @@
+import type { Task } from '../types/task'
 import { defineStore } from 'pinia'
 
-// todo: tipler için bir types.ts veya types/task.ts dosyası oluşturup Task interface'ini oraya taşıyabilirsin.
-interface Task {
-  id: number
-  text: string
-  completed: boolean
-}
+export const useTaskStore = defineStore('task2', () => {
+  const taskList = ref<Task[]>([])
+  const nextId = ref(1)
 
-// todo: bu eski bir yazım yöntemi yeni projelerde bunu göremezsin. doğrusunu sayfanın altına bırakıyorum
-export const useTaskStore = defineStore('task', {
-  state: () => ({
-    taskList: [] as Task[],
-    nextId: 1,
-
-  }),
-  actions: {
-    loadFromLocalStorage() {
-      if (import.meta.client) {
-        const stored = localStorage.getItem('tasks')
-        if (stored !== null) {
-          this.taskList = JSON.parse(stored) as Task[]
-          if (this.taskList.length > 0) {
-            this.nextId = Math.max(...this.taskList.map(t => t.id)) + 1
-          }
-          else {
-            this.taskList = []
-            this.nextId = 1
-          }
+  function loadFromLocalStorage() {
+    if (import.meta.client) {
+      const stored = localStorage.getItem('tasks')
+      if (stored !== null) {
+        taskList.value = JSON.parse(stored) as Task[]
+        if (taskList.value.length > 0) {
+          nextId.value = Math.max(...taskList.value.map(t => t.id)) + 1
+        }
+        else {
+          taskList.value = []
+          nextId.value = 1
         }
       }
-    },
-    saveToLocalStorage(text?: string) {
-      if (text !== undefined) {
-        this.taskList.push({
-          text,
-          completed: false,
-          id: this.nextId++,
-        })
-      }
+    }
+  }
 
-      if (import.meta.client) {
-        localStorage.setItem('tasks', JSON.stringify(this.taskList))
-      }
-    },
+  function saveToLocalStorage(text?: string) {
+    if (text !== undefined) {
+      taskList.value.push({
+        text,
+        completed: false,
+        id: nextId.value++,
+      })
+    }
 
-    checkTask(id: number) {
-      const task = this.taskList.find(t => t.id === id)
-      if (task) {
-        task.completed = !task.completed
-        if (import.meta.client) {
-          localStorage.setItem('tasks', JSON.stringify(this.taskList))
-        }
-      }
-    },
-    deleteTask(id: number) {
-      this.taskList = this.taskList.filter(t => t.id !== id)
+    if (import.meta.client) {
+      localStorage.setItem('tasks', JSON.stringify(taskList.value))
+    }
+  }
+
+  function checkTask(id: number) {
+    const task = taskList.value.find(t => t.id === id)
+    if (task) {
+      task.completed = !task.completed
       if (import.meta.client) {
-        localStorage.setItem('tasks', JSON.stringify(this.taskList))
+        localStorage.setItem('tasks', JSON.stringify(taskList.value))
       }
-    },
-  },
+    }
+  }
+
+  function deleteTask(id: number) {
+    taskList.value = taskList.value.filter(t => t.id !== id)
+    if (import.meta.client) {
+      localStorage.setItem('tasks', JSON.stringify(taskList.value))
+    }
+  }
+
+  return {
+    taskList,
+    nextId,
+
+    loadFromLocalStorage,
+    saveToLocalStorage,
+    checkTask,
+    deleteTask,
+  }
 })
-
-// yeni yazım yöntemi
-
-// export const useTaskStore2 = defineStore('taskt2', () => {
-//   const taskList = ref<Task[]>([])
-
-//   const loadFromLocalStorage = () => {
-
-//   }
-
-//   return {
-//     taskList,
-//     loadFromLocalStorage,
-//   }
-// })
